f83e0f4e625caaa6268fedb01f736a92708d6646
fackco01 <duanhotrong@gmail.com>
2023-11-28 17:08:17 +0700

    First Update

---
+++ HealthExpert/BussinessObject/Model/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObject.Model
+{
+    public class User
+    {
+        [Key]
+        public Guid userId { get; set; }
+        [Required] public string userName { get; set; }
+        [Required] public string firstName { get; set; }
+        [Required] public string lastName { get; set; }
+        [Required] public string phone { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+        [Required]
+        [StringLength(100)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        [Required] public bool gender { get; set; }
+        [Required] public DateTime createDate { get; set; }
+        [Required] public string role { get; set; }
+    }
+}
