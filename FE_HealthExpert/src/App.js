22cc4130a036d791ce1f223405364d97c375b1ae
fackco01 <duanhotrong@gmail.com>
2024-02-28 11:04:49 +0700

    Main FE

---
+++ FE_HealthExpert/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate
+} from "react-router-dom";
+
+import CreateCourse from './page/Course_Admin/create_course';
+import AdminCourseHome from './page//Course_Admin/course_admin_manage';
+import SignUp from './page/Guest/sign_up';
+import ManageUser from './page//Admin/manage_user';
+
+/* IMPORT END */
+
+function Root() {
+  return(
+    <>
+        <Routes>
+          <Route
+          path = "/"
+          element = {<AdminCourseHome/>}
+          />
+          
+          <Route
+          path = "/createCourse"
+          element = {<CreateCourse/>}
+          />
+
+          <Route
+          path = "/adminCourseHome"
+          element = {<AdminCourseHome/>}
+          />
+
+          <Route 
+          path = "/signUp"
+          element = {<SignUp/>}
+          />
+
+          <Route 
+          path = "/manageUser"
+          element = {<ManageUser/>}
+          />
+        </Routes>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+      <Root />
+    </Router>
+  );
+}
+
+export default App;
