b157d2762768abc822c1258e1e18e0d302be3809
nhathoang244 <hoangtnde150305@fpt.edu.vn>
2024-03-10 18:37:12 +0700

    Update FE

--- FE_HealthExpert/src/App.js
+++ FE_HealthExpert/src/App.js
@@ -7,12 +7,15 @@ import SignIn from "./page/Auth/SignIn";
 import { useRoutes } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import SignUp from "./page/Auth/SignUp";
+import Yoga from "./page/Services/yoga";
 
 export default function App() {
   return (
     <Routes>
       {/* <Route path="/" element={<Layout />}> */}
       <Route path="/home" element={<Home />} />
+      <Route path="/yoga" element={<Yoga />} />
+
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
 
---
+++ FE_HealthExpert/src/page/Services/yoga.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { GithubOutlined, LogoutOutlined } from "@ant-design/icons";
+import { Helmet } from "react-helmet";
+import { Button } from "antd";
+import Header from "../../components/Header";
+
+export default function yoga() {
+  // const dispatch = useDispatch();
+
+  return (
+    <>
+      <div className="home-page">
+        <Header />
+      </div>
+    </>
+  );
+}
